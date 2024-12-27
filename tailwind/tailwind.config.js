bd20cb173d87ccb6bf83ab0909abb4eb6cadc29b
sanjana <sanjanakazisupti@gmail.com>
2024-12-27 20:56:58 +0600

    Tailwind CSS Basic

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // `src` folder e shob HTML o JS files cover korbe
+    './*.{html,js}'         // Root folder e thaka HTML o JS files cover korbe
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
